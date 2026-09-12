50b1e7c9b8e69f7364e7acc1963b5e98c65d9995
mctwm2pkzg-netizen <mctwm2pkzg@privaterelay.appleid.com>
2026-09-12 19:06:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    arrivano appena pubblicati; se non c'è rete si usa la copia in cache.
    Icone e manifest: prima cache, poi rete.
    Per forzare un aggiornamento della cache basta cambiare VERSION. */
-const VERSION = "pw-2026-09-12-8";
+const VERSION = "pw-2026-09-12-9";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png", "./icon-512-maskable.png"];
 
 self.addEventListener("install", (e) => {
